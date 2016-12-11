b8698964bfe55122f53cb583c7b7ce9470b17941
Kylmakalle <iceman9831@gmail.com>
2016-12-11 20:02:08 +0300

    sempai tr

--- timecountdown.py
+++ timecountdown.py
@@ -82,6 +82,8 @@ def handle_text(message):
         bot.send_message(message.chat.id, "физика круто", reply_to_message_id=message.message_id)    
     if message.text == "Ботай":
         bot.send_message(message.chat.id, "или умри")
+    if message.text == "Андрей":
+        bot.send_message(message.chat.id, "расскажи как ты стал джуном")    
 
 
 
