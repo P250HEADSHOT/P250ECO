8847f78b1212090bfbfb388fc8ba64cf234237fa
Kylmakalle <iceman9831@gmail.com>
2016-12-11 19:41:44 +0300

    Update timecountdown.py

--- timecountdown.py
+++ timecountdown.py
@@ -80,7 +80,7 @@ def handle_text(message):
         bot.send_message(message.chat.id, "Неделя", reply_to_message_id=message.message_id)
     if message.text == "Скажи физика круто":
         bot.send_message(message.chat.id, "физика круто", reply_to_message_id=message.message_id)
-    else bot.send_message(message.chat.id, "Чего тебе?", reply_to_message_id=message.message_id)    
+    elif bot.send_message(message.chat.id, "Чего тебе?", reply_to_message_id=message.message_id)    
 
 
 
