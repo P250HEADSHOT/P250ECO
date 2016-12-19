b6b7e08cc1c489bc5f3698a6cae520c8229f7c4d
Kylmakalle <iceman9831@gmail.com>
2016-12-19 18:19:11 +0300

    timezone fix

--- timecountdown.py
+++ timecountdown.py
@@ -9,7 +9,7 @@ out = datetime.timedelta(now, rus)
 print(out)'''
 #continuous-integration
 import telebot
-
+import pytz
 technoconf = -1001070076534
 me = 94026383
 sup = "BQADAgADrQEAAm29TQUoveU--qPBlAI"
@@ -23,14 +23,14 @@ rus = datetime.datetime.strptime("09.06.2017 10:00", FMT)
 math = datetime.datetime.strptime("02.06.2017 10:00", FMT)
 phys = datetime.datetime.strptime("07.06.2017 10:00", FMT)
 it = datetime.datetime.strptime("29.05.2017 10:00", FMT)
-
+tz=pytz.timezone("Europe/Moscow")
 # print('Русский через',c)
 # bot.send_message(technoconf, "Русский через")
 # bot.send_message(technoconf, drus)
 
 @bot.message_handler(commands=['rus'])
 def handle_text(message):
-    now = datetime.datetime.now()
+    now = datetime.datetime.now(tz)
     drus = rus - now
     bot.send_message(message.chat.id, "До Русича осталось {} дней, {} часов {} минут {} секунд.".format(drus.days, drus.seconds//3600, drus.seconds%3600//60, drus.seconds%60)
 , reply_to_message_id=message.message_id)
@@ -38,21 +38,21 @@ def handle_text(message):
 
 @bot.message_handler(commands=['math'])
 def handle_text(message):
-    now = datetime.datetime.now()
+    now = datetime.datetime.now(tz)
     dmath = math - now
     bot.send_message(message.chat.id, "До Матеши осталось {} дней, {} часов {} минут {} секунд.".format(dmath.days, dmath.seconds//3600, dmath.seconds%3600//60, dmath.seconds%60), reply_to_message_id=message.message_id)
 
 
 @bot.message_handler(commands=['phys'])
 def handle_text(message):
-    now = datetime.datetime.now()
+    now = datetime.datetime.now(tz)
     dphys = phys - now
     bot.send_message(message.chat.id, "До Физона осталось {} дней, {} часов {} минут {} секунд.".format(dphys.days, dphys.seconds//3600, dphys.seconds%3600//60, dphys.seconds%60), reply_to_message_id=message.message_id)
 
 
 @bot.message_handler(commands=['ikt'])
 def handle_text(message):
-    now = datetime.datetime.now()
+    now = datetime.datetime.now(tz)
     dit = it - now
     bot.send_message(message.chat.id, "До ИКТ осталось {} дней, {} часов {} минут {} секунд.".format(dit.days, dit.seconds//3600, dit.seconds%3600//60, dit.seconds%60), reply_to_message_id=message.message_id)
 
@@ -60,20 +60,20 @@ def handle_text(message):
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     if ("скока до русича" in str.lower(message.text)) or  ("cкока до русского" in str.lower(message.text)) or ("cколько до русича" in str.lower(message.text)) or ("cколько до русского" in str.lower(message.text)):
-        now = datetime.datetime.now()
+        now = datetime.datetime.now(tz)
         drus = rus - now
         bot.send_message(message.chat.id, "До Русича осталось {} дней, {} часов {} минут {} секунд.".format(drus.days, drus.seconds//3600, drus.seconds%3600//60, drus.seconds%60)
 , reply_to_message_id=message.message_id)
     if ("cкока до матеши" in str.lower(message.text)) or ("сколько до матеши" in str.lower(message.text)) or ("скока до математики" in str.lower(message.text)) or ("сколько до математики" in str.lower(message.text)):
-        now = datetime.datetime.now()
+        now = datetime.datetime.now(tz)
         dmath = math - now
         bot.send_message(message.chat.id, "До Матеши осталось {} дней, {} часов {} минут {} секунд.".format(dmath.days, dmath.seconds//3600, dmath.seconds%3600//60, dmath.seconds%60), reply_to_message_id=message.message_id)
     if ("скока до физеки" in str.lower(message.text)) or ("сколько до физеки" in str.lower(message.text)) or ("скока до физеки" in str.lower(message.text)):
-        now = datetime.datetime.now()
+        now = datetime.datetime.now(tz)
         dphys = phys - now
         bot.send_message(message.chat.id, "До Физона осталось {} дней, {} часов {} минут {} секунд.".format(dphys.days, dphys.seconds//3600, dphys.seconds%3600//60, dphys.seconds%60), reply_to_message_id=message.message_id)
     if ("скока до икт" in str.lower(message.text)) or ("сколько до икт" in str.lower(message.text)):
-        now = datetime.datetime.now()
+        now = datetime.datetime.now(tz)
         dit = it - now
         bot.send_message(message.chat.id, "До ИКТ осталось {} дней, {} часов {} минут {} секунд.".format(dit.days, dit.seconds//3600, dit.seconds%3600//60, dit.seconds%60), reply_to_message_id=message.message_id)
     if "люблю тебя" in str.lower(message.text):
